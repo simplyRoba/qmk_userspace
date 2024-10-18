94038f6087eb8cef4d8693d36bf8d47d50ace491
harvey <harvey@splitkb.com>
2024-10-18 15:28:17 +0200

    remove encoder quadrature as it's default

--- users/halcyon_modules/hlc_encoder/rules.mk
+++ users/halcyon_modules/hlc_encoder/rules.mk
@@ -1,6 +1,4 @@
 CURRENT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 
-ENCODER_DRIVER ?= quadrature
-
 SRC += $(CURRENT_DIR)/hlc_encoder.c $(CURRENT_DIR)/hlc_encoder_matrix.c
 CONFIG_H += $(CURRENT_DIR)/config.h
