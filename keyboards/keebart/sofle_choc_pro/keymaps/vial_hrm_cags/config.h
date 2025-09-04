5659ef285fb12fd034b537787ab0d8ea98117d6b
Axel <git@simplyroba.de>
2025-09-04 19:19:42 +0200

    add vial config for keebart sofle

---
+++ keyboards/keebart/sofle_choc_pro/keymaps/vial_hrm_cags/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID { 0x4F, 0x2D, 0x5A, 0x8A, 0x49, 0x7C, 0xDF, 0x1D }
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 190
